43184a8d29a98d723756b578fd6f6c0099349b02
ravi2615 <67025166+ravi2615@users.noreply.github.com>
2020-09-07 01:24:45 -0700

    first commit

---
+++ build/precache-manifest.0738c84b86f7b962b142c0f02a4450f2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cdc66619d38adfe1b40d0ee3f197399",
+    "url": "/painting/index.html"
+  },
+  {
+    "revision": "e05c72a4aacad7223a5c",
+    "url": "/painting/static/css/2.fc5756f1.chunk.css"
+  },
+  {
+    "revision": "5b784e89e400e6f98503",
+    "url": "/painting/static/css/main.d03b43d0.chunk.css"
+  },
+  {
+    "revision": "e05c72a4aacad7223a5c",
+    "url": "/painting/static/js/2.15528989.chunk.js"
+  },
+  {
+    "revision": "7c31314e008d1a59ddfae5300ece5508",
+    "url": "/painting/static/js/2.15528989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b784e89e400e6f98503",
+    "url": "/painting/static/js/main.c95ce944.chunk.js"
+  },
+  {
+    "revision": "7db107e71c979ac3ad4f",
+    "url": "/painting/static/js/runtime-main.fcff1a69.js"
+  },
+  {
+    "revision": "461080d46251e63a1fea084d5ecf1c13",
+    "url": "/painting/static/media/7d471656-5ffa-411d-b3a7-f7ee34868d94 (1).461080d4.jpg"
+  },
+  {
+    "revision": "6bf67e4e09b3e09a9f2255653a446f1b",
+    "url": "/painting/static/media/slide-1.6bf67e4e.png"
+  }
+]);
